0b3624cb5d4f86f098c4d846e7df40f6521f5952
Jakub Głazik <jglazik@sygnity.pl>
2017-06-09 21:23:15 +0200

    Simple event implementation (turn number random for now).

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,3 +23,53 @@ function update(j) {
 - zmiana pozycji tokena: https://stackoverflow.com/questions/12744928/in-jquery-how-can-i-set-top-left-properties-of-an-element-with-position-values
   $('#token').css({top: 200, left: 200, position:'absolute'});
 */
+
+function getTurnNumber() {
+  // random for now
+  return Math.floor(Math.random()*12+1);
+}
+
+function roll1d6() {
+  return Math.floor(Math.random()*6+1);
+}
+
+function checkForEvent() {
+  var turnNo = getTurnNumber();
+  console.log("turnNo: " + turnNo);
+  if(turnNo%2==0) {
+  //if(turnNo==2 || turnNo==4 || turnNo==6 || turnNo==8 || turnNo==10 || turnNo==12) {
+    var roll = roll1d6();
+    console.log('event roll: ' + roll);
+    switch (roll) {
+      case 1: case 2: eventInspiration(); break;
+      case 3:         eventGlitch(); break;
+      case 4: case 5: eventPadExplosion(); break;
+      case 6:         eventSpecial(turnNo); break;
+      default:
+    }
+  } else {
+    console.log("not event turn!");
+  }
+}
+
+function eventInspiration() {
+  alert('Inspiration!');
+}
+
+function eventGlitch() {
+  alert('Glitch!');
+}
+
+function eventPadExplosion() {
+  alert('Pad explosion/Space debris!');
+}
+
+function eventSpecial(turnNo) {
+  if(turnNo==2 || turnNo==12) {           // blue event
+    alert('Election year!')
+  } else if (turnNo==4 || turnNo==6) {    // yellow event
+    alert('Budget cuts!')
+  } else if (turnNo==8 || turnNo==10) {   // red event
+    alert('Solar flare! Strength: ' + roll1d6())
+  }
+}
