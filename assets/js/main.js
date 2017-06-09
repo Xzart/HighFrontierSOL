2fcee61dec785db7a01fd746f8655cb066556e50
Jakub Głazik <jglazik@sygnity.pl>
2017-06-09 20:45:41 +0200

    Dwa pomysly na dalszy rozwoj.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,12 @@
 function initialize() {
 
-    $("#token").draggable({ containment: "#sunBox", scroll: false });
+    $("#token").draggable({ containment: "#sunBox", scroll: false,
+        stop: function(event, ui) {
+          var top = $('#token').css('top');
+          var left = $('#token').css('left');
+          console.log('dragged to top=' + top + ' and left='+ left + '');
+        }
+    });
 
 };
 
@@ -12,3 +18,15 @@ function update(j) {
     var n = parseInt(j.text(), 10);
     j.text(n+1);
 }
+
+/* notatki
+- zmiana pozycji tokena: https://stackoverflow.com/questions/12744928/in-jquery-how-can-i-set-top-left-properties-of-an-element-with-position-values
+  $('#token').css({top: 200, left: 200, position:'absolute'});
+
+- nasłuchiwanie za zmianą pozycji tokena: https://stackoverflow.com/questions/25587313/jquery-ui-draggable-position-simultaneously
+  $("#token").draggable({ containment: "#sunBox", scroll: false
+    drag: functi
+  });
+
+
+*/
