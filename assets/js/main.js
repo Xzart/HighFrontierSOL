b1ea985000e939471657133117db3c21e9a267e8
Jakub Głazik <jglazik@sygnity.pl>
2017-06-09 20:18:48 +0200

    Bug fixed!

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,12 @@
 function initialize() {
-        
+
     $("#token").draggable({ containment: "#sunBox", scroll: false });
-    
+
 };
 
-function sunButton() {
-    //$("span.sunButton").onclick(document.getElementById("sunButton").innerHTML="Button pressed");
-    $("#sunButton").click(function() {
-        update($("#TurnNumber"));
-    } );
-}
+$("#sunButton").click(function() {
+    update($("#TurnNumber"));
+} );
 
 function update(j) {
     var n = parseInt(j.text(), 10);
